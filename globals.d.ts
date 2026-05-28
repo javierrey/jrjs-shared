2563fdbd61e0026fd6b60933931cbe144accddfe
unknown <javier.rey.eu@gmail.com>
2026-05-28 17:07:34 +0100

    update tsconfig

--- globals.d.ts
+++ globals.d.ts
@@ -1,3 +1,4 @@
 // jrjs global.d.ts
 // declare module 'jrjs-shared/packages/lib/sys/run.js';
+// declare var process: Record<PropertyKey, any>;
 // declare var globalConfig: Record<PropertyKey, unknown>;
